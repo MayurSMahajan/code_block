36f135609f17bf069ad610615989ae69d3bd1a1f
MayurSMahajan <mayur23smahajan@gmail.com>
2023-08-11 22:02:09 +0530

    chore: change title

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Code Block Demo',
       localizationsDelegates: const [
         ...AppLocalizations.localizationsDelegates,
         AppFlowyEditorLocalizations.delegate,
